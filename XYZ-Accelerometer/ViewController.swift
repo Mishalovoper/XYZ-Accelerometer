77b152782718842dc2bd00a45cad23f572a7d3ce
MISHAL ALHAJRI <mishalalhajri@MISHALs-MacBook-Air.local>
2019-07-19 19:46:36 +0300

    Build the User Interface

---
+++ XYZ-Accelerometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XYZ-Accelerometer
+//
+//  Created by MISHAL ALHAJRI on 16/11/1440 AH.
+//  Copyright © 1440 MISHAL ALHAJRI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
